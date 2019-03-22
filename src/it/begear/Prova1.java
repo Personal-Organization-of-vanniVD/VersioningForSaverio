18415afcc2ce746253425b73ddcbe3a5c2234ac4
Vanni <vanniVD@yahoo.com>
2019-03-22 11:59:09 +0100

    Aggiunti metodi getter and setter

--- src/it/begear/Prova1.java
+++ src/it/begear/Prova1.java
@@ -6,5 +6,39 @@ public class Prova1 {
 	private int y;
 	private String s;
 	private boolean tof;
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public String getS() {
+		return s;
+	}
+	
+	public void setS(String s) {
+		this.s = s;
+	}
+	
+	public boolean isTof() {
+		return tof;
+	}
+	
+	public void setTof(boolean tof) {
+		this.tof = tof;
+	}
 
+	
+	
 }
